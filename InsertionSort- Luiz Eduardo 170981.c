11709e0c2e4cd9657ab3daf5d6a7e3f4b19590d9
Luiz Eduardo <162394772+LuizEduSiqueira@users.noreply.github.com>
2024-12-09 20:27:02 -0300

    Update InsertionSort- Luiz Eduardo 170981.c

--- InsertionSort- Luiz Eduardo 170981.c
+++ InsertionSort- Luiz Eduardo 170981.c
@@ -5,13 +5,12 @@ AED 1 Prof. Arlindo - 2024.2*/
 #include <stdlib.h>
 #include <time.h>
 
-// Ordenação InsertionSort
+// Ordenação de um array com Insertion Sort
 void insertionSort(int arr[], int n) {
     for (int i = 1; i < n; i++) {
         int chave = arr[i];
         int j = i - 1;
 
-        // Move os elementos maiores que uma posição à frente
         while (j >= 0 && arr[j] > chave) {
             arr[j + 1] = arr[j];
             j = j - 1;
@@ -20,7 +19,7 @@ void insertionSort(int arr[], int n) {
     }
 }
 
-// Print array
+// Print Array
 void printArray(int arr[], int n) {
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
@@ -31,22 +30,29 @@ void printArray(int arr[], int n) {
 int main() {
     int n;
 
-    printf("Digite o tamanho do array: ");
+    // Lê o tamanho do vetor
+    printf("Digite o tamanho do vetor: ");
     scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("O tamanho do vetor deve ser maior que 0.\n");
+        return 1;
+    }
 
     int arr[n];
 
     // Inicializa o gerador de números aleatórios
     srand(time(NULL));
 
-    // Preenche o array com números aleatórios
+    // Preenche o vetor com números aleatórios entre 0 e n-1
     for (int i = 0; i < n; i++) {
-        arr[i] = rand() % 100; // Gera números aleatórios entre 0 e 99
+        arr[i] = rand() % n;
     }
 
     printf("Array gerado:\n");
     printArray(arr, n);
 
+    // Ordena o array
     insertionSort(arr, n);
 
     printf("Array ordenado:\n");
