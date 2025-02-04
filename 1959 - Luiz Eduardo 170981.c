75cfc89c8b1b21d98052943b85befc2c64ea6cb8
Luiz Eduardo <162394772+LuizEduSiqueira@users.noreply.github.com>
2025-02-04 10:59:22 -0300

    Update 1959 - Luiz Eduardo 170981.c

--- 1959 - Luiz Eduardo 170981.c
+++ 1959 - Luiz Eduardo 170981.c
@@ -1,8 +1,8 @@
 /*Luiz eduardo garcia de Siqueira 170981 turma NA
 AED 1 Prof. Arlindo - 2024.2*/
 
-main(){
+int main(){
 	long long int n, l;
 	scanf("%lld",&n);scanf("%lld",&l);
     printf("%lld\n",(n*l));
-system("pause");}
+return 0;}
