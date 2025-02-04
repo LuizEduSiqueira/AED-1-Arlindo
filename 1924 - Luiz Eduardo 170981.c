e96020524f3563d8c6ad7d6a00db8ae3de7d8fac
Luiz Eduardo <162394772+LuizEduSiqueira@users.noreply.github.com>
2025-02-04 11:00:22 -0300

    Update 1924 - Luiz Eduardo 170981.c

--- 1924 - Luiz Eduardo 170981.c
+++ 1924 - Luiz Eduardo 170981.c
@@ -2,7 +2,7 @@
 AED 1 Prof. Arlindo - 2024.2*/
 
 #include <stdio.h>
-main(){
+int main(){
 	int quantidade,i;
     char curso_1[1001],curso_certo[]="Ciencia da Computacao";
     scanf("%d",&quantidade);
@@ -10,4 +10,4 @@ main(){
         scanf("%s",&curso_1);
     }
     printf("%s\n",curso_certo);
-system("pause");}
+return 0;}
