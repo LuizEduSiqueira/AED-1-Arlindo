b951f3daba120b51ad1e8d16e2d167ccfdf97e84
Luiz Eduardo <162394772+LuizEduSiqueira@users.noreply.github.com>
2025-02-04 10:57:13 -0300

    Update 2029 - Luiz Eduardo 170981.c

--- 2029 - Luiz Eduardo 170981.c
+++ 2029 - Luiz Eduardo 170981.c
@@ -5,7 +5,7 @@ AED 1 Prof. Arlindo - 2024.2*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-main(){
+int main(){
     double area=0,diametro, volume;
 		while(scanf("%lf %lf",&volume,&diametro)!=EOF){
                 /* > H = Volume/Pi*(Raio)^2 < */
@@ -13,4 +13,4 @@ main(){
                 /* > A = Pi*(Raio)^2 < */
             printf("AREA = %.2lf\n",PI*(pow((diametro/2),2)));
         }
-system("pause");}
+return 0;}
